5453ed525f832f288016477540c7e9a0a6c4876b
Azhari S <azhari.subroto@gmail.com>
2022-07-21 08:35:33 +0700

    fix(button): rename button component

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,7 +4,7 @@ import TextField from '@/components/TextField';
 import DropZone from '@/components/DropZone';
 import { tw } from 'twind/css';
 import { handleDownloadImage } from '@/lib/helpers';
-import Button from '@/components/Buttonn';
+import Button from '@/components/Button';
 
 /**
  * Home page
